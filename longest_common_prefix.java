457ef62c5e2bf94ac4da54851928175b0fe58a92
avimishra18 <mishra.avi18@gmail.com>
2023-06-09 00:15:57 +0530

    🧼 Longest Common Prefix in Str Arr

--- longest_common_prefix.java
+++ longest_common_prefix.java
@@ -1,22 +1,14 @@
 //https://leetcode.com/problems/longest-common-prefix/solutions/3273176/python3-c-java-19-ms-beats-99-91/
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(strs[0]);
-        for(int i=1;i<strs.length && sb.length() !=0;i++){
-            String curr = strs[i];
-            if(sb.length()>curr.length()){
-                sb.setLength(curr.length());
-            }
-            for(int j=0;j<sb.length();j++){
-               if(curr.charAt(j) != sb.charAt(j)){
-                    if(sb.length() > 0){
-                        sb.setLength(j);
-                    }
-                   break;
-               } 
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        for(int i=0; i<Math.min(first.length(),last.length());i++){
+            if(first.charAt(i) != last.charAt(i)){
+                return first.substring(0,i);
             }
         }
-        return sb.toString();
+        return first;
     }
 }
