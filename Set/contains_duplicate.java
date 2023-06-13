26d983c6dfc2db6cc43c466c26bde0bc5a8660aa
avimishra18 <mishra.avi18@gmail.com>
2023-06-13 22:45:03 +0530

    🥓 Contains duplicate

---
+++ Set/contains_duplicate.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/contains-duplicate/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>(); 
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+            }else{
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
