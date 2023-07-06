c6afde98a67b18906b92d8fd4634f387a20e142a
avimishra18 <mishra.avi18@gmail.com>
2023-07-06 23:49:59 +0530

    🛣️ Distance Traveled

---
+++ Maths/total_distance_travelled.java
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/total-distance-traveled
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+        return (mainTank + Math.min((mainTank - 1) / 4, additionalTank)) * 10;
+    }
+}
