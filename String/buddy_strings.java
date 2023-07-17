9a87c539a7c63beb74a36c42437c2480cf3c2ab0
avimishra18 <mishra.avi18@gmail.com>
2023-07-18 00:26:48 +0530

    👭 buddy string

--- String/buddy_strings.java
+++ String/buddy_strings.java
@@ -1,14 +1,32 @@
 // https://leetcode.com/problems/buddy-strings/
 class Solution {
-    public boolean buddyStrings(String A, String B) {
-          if (A.length() != B.length()) return false;
-        if (A.equals(B)) {
-            Set<Character> s = new HashSet<Character>();
-            for (char c : A.toCharArray()) s.add(c);
-            return s.size() < A.length();
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+        if(s.equals(goal)){
+            HashSet<Character> set = new HashSet<>();
+            for(int i=0;i<s.length();i++){
+                if(set.contains(s.charAt(i))) return true;
+                set.add(s.charAt(i));
+            }
+            return false;
         }
-        List<Integer> dif = new ArrayList<>();
-        for (int i = 0; i < A.length(); ++i) if (A.charAt(i) != B.charAt(i)) dif.add(i);
-        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
+        
+        int count = 0;
+        char[] diff = new char[2];
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=goal.charAt(i)){
+                if(++count==1){
+                    diff[0] = s.charAt(i);
+                    diff[1] = goal.charAt(i);
+                }
+                if(count==2){
+                    if(diff[0]!=goal.charAt(i) || diff[1]!=s.charAt(i)){
+                        return false;
+                    }
+                }
+                if(count == 3) return false;
+            }
+        }
+        return count != 1;
     }
 }
