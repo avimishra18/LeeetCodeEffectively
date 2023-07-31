6a68b42a85324422f32a7b6ad384bc8b813663da
avimishra18 <mishra.avi18@gmail.com>
2023-07-31 23:23:11 +0530

    ➕ Path Sum

--- Tree/path_sum.java
+++ Tree/path_sum.java
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/path-sum
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,13 +14,10 @@
  *     }
  * }
  */
- 
-public class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if(root == null) return false;
-    
-        if(root.left == null && root.right == null && sum - root.val == 0) return true;
-    
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+        if(root.left==null && root.right==null) return targetSum-root.val==0;
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
     }
 }
