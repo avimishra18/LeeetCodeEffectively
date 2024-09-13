ec98c048f1ba733110049e1113ac4d1e176cf696
Avi Mishra <mishra.avi18@gmail.com>
2024-09-13 23:27:32 +0530

    Ⅸ roman-to-integer

--- String/roman-to-integer.java
+++ String/roman-to-integer.java
@@ -1,32 +1,21 @@
 // https://leetcode.com/problems/roman-to-integer
 class Solution {
   public int romanToInt(String s) {
-      HashMap<Character,Integer> map = new HashMap<>();
-      map.put('I',1);
-      map.put('V',5);
-      map.put('X',10);
-      map.put('L',50);
-      map.put('C',100);
-      map.put('D',500);
-      map.put('M',1000);
-
-      int i=0,sum=0;
-      for(i=0;i<s.length()-1;i++){
-          char curr = s.charAt(i);
-          char next = s.charAt(i+1);
-          if((curr == 'I' && (next == 'V' || next == 'X')) || 
-              (curr == 'X' && (next == 'L' || next == 'C')) ||
-              (curr == 'C' && (next == 'D' || next == 'M'))
-          ){
-              sum+=map.get(next)-map.get(curr);
-              i++;
-          }else{
-              sum+=map.get(curr);
+      int prev=0,sum=0,num=0;
+      for(int i=s.length()-1;i>=0;i--){
+          switch(s.charAt(i)){
+              case 'M' -> num=1000;
+              case 'D' -> num=500;
+              case 'C' -> num=100;
+              case 'L' -> num=50;
+              case 'X' -> num=10;
+              case 'V' -> num=5;
+              case 'I' -> num=1;
           }
-      }
-      if(i<s.length()){
-          sum+=map.get(s.charAt(i));
-      }
+          if(prev>num) sum-=num; 
+          else sum+=num;
+          prev=num;
+      } 
       return sum;
   }
 }
@@ -36,6 +25,7 @@ Problem:-
 Solution:-
   1. Hashmap to store the values
   2. Explicit if check for 6 instances
+  3. Second approach :- iterate array in reverse check prev value
 Missed Cases:-
   1. Was not having i<s.length() check for the last character 
       IV then there will be an extra i++ 
